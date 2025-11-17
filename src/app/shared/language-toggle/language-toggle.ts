24515ff0d3a3680e4fc9390bda4875909369b174
taektu_coding <taektu.coding@gmail.com>
2025-11-17 11:08:51 +0100

    feat(ui): Replace language toggle text with flag icons

--- src/app/shared/language-toggle/language-toggle.ts
+++ src/app/shared/language-toggle/language-toggle.ts
@@ -1,10 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { IconFlagGermanyComponent } from '@app/icons/icon-flag-germany/icon-flag-germany';
+import { IconFlagUsaComponent } from '@app/icons/icon-flag-usa/icon-flag-usa';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-toggle',
-  imports: [NgClass],
+  imports: [NgClass, IconFlagGermanyComponent, IconFlagUsaComponent],
   templateUrl: './language-toggle.html',
   styleUrl: './language-toggle.css',
 })
